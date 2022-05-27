caa0935f43c5ef44fdd92a19fe49f97ddd0ada13
VishavKamboj01 <vishavbehar@gmail.com>
2022-05-27 13:16:36 +0530

    Add new Chart

--- src/components/Sections/BackToSections.jsx
+++ src/components/Sections/BackToSections.jsx
@@ -3,6 +3,7 @@ import { TagContainer, Title, TitleContainer, UpArrow } from "./styles";
 import arrow from "../../images/up-arrow.png";
 import Link from "react-scroll/modules/components/Link";
 export default function BackToSections() {
+  
   return (
     <Link to="Sections" smooth={true}>
       <TagContainer data-aos="fade-right">
